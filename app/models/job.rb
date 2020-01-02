aada7a4812ed92cbae53fe67c973b2aacb175f10
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-02 16:57:20 +0700

    Remove redundant lines

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,11 +6,4 @@ class Job < ApplicationRecord
 
   has_many :industry_jobs
   has_many :saved_jobs
-
-  def self.import(file)
-
-    CSV.foreach(file.path, headers: true) do |row|
-
-    end
-  end
 end
