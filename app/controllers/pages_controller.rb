434a62a5b5008c5348d37ec49671c4d1d72547b8
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-06 11:29:07 +0700

    Clean up code used for SQL queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @latest_jobs = Job.order(updated_at: :desc).limit(5)
+
+    @top_cities = City.all.map { |c| [c, c.jobs.count] }.sort_by(&:second).reverse.take(11)
+
+    @top_industries = Industry.all.map { |i| [i, i.industry_jobs.count] }.sort_by(&:second).reverse.take(11)
   end
 
   def search
-
   end
 end
