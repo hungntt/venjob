5413845bf1141baf2009f4caf78d9038b24a93d5
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-06 17:34:54 +0700

    City list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'cities/index'
+  get 'cities/show'
   root "pages#home"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cities
 end
