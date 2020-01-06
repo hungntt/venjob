f0b9d5931049e37f05b1aa20174605de77f30b53
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-06 14:13:49 +0700

    Use Config gem to config all constants

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @latest_jobs = Job.order(updated_at: :desc).limit(5)
+    @latest_jobs = Job.order(updated_at: :desc).limit(Settings.home_page.job_list_limit)
 
-    @top_cities = City.all.map { |c| [c, c.jobs.count] }.sort_by(&:second).reverse.take(11)
+    @top_cities = City.all.map { |c| [c, c.jobs.count] }.sort_by(&:second).reverse.take(Settings.home_page.city_list_limit)
 
-    @top_industries = Industry.all.map { |i| [i, i.industry_jobs.count] }.sort_by(&:second).reverse.take(11)
+    @top_industries = Industry.all.map { |i| [i, i.industry_jobs.count] }.sort_by(&:second).reverse.take(Settings.home_page.industry_list_limit)
   end
 
   def search
