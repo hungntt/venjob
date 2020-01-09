fb27667c73100a0003026ea9627cfb10b3ba5ec0
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-09 14:00:04 +0700

    Put the navigation into partial and handle form correctly

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,8 @@
 class RequestsController < ApplicationController
   before_action :load_job, only: %i[new confirm]
+  before_action :new_request, only: %i[confirm done]
+  before_action :check_input, only: %i[confirm done]
+  after_action :set_request_job_id, only: %i[new]
 
   def index
   end
@@ -9,8 +12,6 @@ class RequestsController < ApplicationController
   end
 
   def done
-    @request = Request.new(request_params)
-    @request.job_id = params[:job_id]
     if @request.save
       flash.now[:success] = "Successfully apply. Check mail confirmation at #{@request.email}"
       @request.send_confirmation_email
@@ -28,13 +29,19 @@ class RequestsController < ApplicationController
                else
                  Request.new
                end
+
   end
 
   def confirm
-    @request = Request.new(request_params)
+
   end
 
   private
+
+  def new_request
+    @request = Request.new(request_params)
+    set_request_job_id
+  end
 
   def load_job
     @job = Job.find_by(id: params[:job_id])
@@ -44,4 +51,14 @@ class RequestsController < ApplicationController
     params.require(:request).permit(:fname, :lname, :email, :cv)
   end
 
+  def set_request_job_id
+    @request.job_id = params[:job_id]
+  end
+
+  def check_input
+    unless @request.valid?
+      flash[:danger] = "Missing requirements: " + @request.errors.full_messages.join(", ") + ". Please click 'Edit' to fill the form again."
+      redirect_to new_job_request_path
+    end
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,9 @@ class Request < ApplicationRecord
   delegate :name, to: :job, prefix: true
 
   validates :fname, presence: true
+  validates :lname, presence: true
+  validates :email, presence: true
+  validates :cv, presence: true
 
   def send_confirmation_email
     RequestMailer.request_confirmation(self).deliver_now
