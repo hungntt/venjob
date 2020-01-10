192c7d71699db19ad254da4b2135819ada8c4d5e
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-10 15:14:36 +0700

    Remove load_user in History

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,15 +1,5 @@
 class HistoriesController < ApplicationController
-  before_action :load_user, only: :index
-
   def index
-    @jobs = @user.histories
-  end
-
-
-
-  private
-
-  def load_user
-    @user = current_user
+    @jobs = current_user.histories
   end
 end
