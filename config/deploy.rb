96831b49e1464bff6e203f3338f0446b271c62d4
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2020-01-31 13:49:41 +0700

    Initialize deploy app more

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
+set :application, "venjob"
+set :repo_url, "git@gitlab.zigexn.vn:hungntt/venjob.git"
+set :branch, "create-admin"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -19,7 +19,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for :pty is false
 # set :pty, true
-
+append :linked_files, "config/master.key"
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 
